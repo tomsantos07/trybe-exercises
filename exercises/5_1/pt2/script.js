66ab161e715f32cec9b3c8191b3e0d6e7ee5d3bc
tomsantos07 <xotmsantosx@live.com>
2021-03-15 18:50:41 -0300

    corrigido

---
+++ exercises/5_1/pt2/script.js
@@ -0,0 +1,19 @@
+// Administrador do Tempo da Trybe
+document.getElementsByTagName("h1")[0].style.background = '#3CB371';
+
+// Urgente e Importante / Urgente e Não-importante
+document.getElementsByTagName("h3")[0].style.background = '#704488';
+document.getElementsByTagName("h3")[1].style.background = '#704488';
+
+// Não-Urgente e Importante / Não-Urgente e Não-Importante
+document.getElementsByTagName('h3')[2].style.background = 'black';
+document.getElementsByTagName('h3')[3].style.background = 'black';
+
+// Background lado esquerdo
+document.getElementsByClassName('emergency-tasks')[0].style.background = '#e99369';
+
+// Background lado direito
+document.getElementsByClassName('no-emergency-tasks')[0].style.background = 'yellow';
+
+
+
